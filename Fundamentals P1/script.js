cf4d99604ce1d5b86c11291130ceb59b4926b18c
Naren <naren.p.2018.cse@ritchennai.edu.in>
2021-02-24 15:24:09 +0530

    First commit

---
+++ Fundamentals P1/script.js
@@ -0,0 +1,15 @@
+let js = 'amazing'; //Variable
+console.log(40 + 8 + 53); //Values
+
+const firstName = "Naren DevJs";  // Variable in camelcase
+console.log(firstName);
+
+let firstPerson = "javascript";
+console.log(firstPerson);
+
+let jonas_matilda = "developers";
+let PI = 3.1415;
+
+let myFirstSchool = "DAV";
+let myCollege = "RIT";
+        
