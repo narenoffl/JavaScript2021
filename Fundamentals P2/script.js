74b7d8cdeadfe34cb22fb2313e14f42d5f463cc3
Naren <naren.p.2018.cse@ritchennai.edu.in>
2021-02-27 14:49:49 +0530

    Arrow Functions

--- Fundamentals P2/script.js
+++ Fundamentals P2/script.js
@@ -1,2 +1,56 @@
 //Activating Strict Mode
 'use strict';
+
+/*Functions
+function logger() {
+    console.log("My name is Naren"); //This function prints only message but doesn't return any value
+}
+
+//Calling, running or invoking a function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice =`Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,2);
+console.log(appleJuice);
+console.log(fruitProcessor(5,5));
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice); */
+
+
+/*Function Declarations vs. Expressions
+function calcAge1(birthYear){          //Function Declaration
+    const age = 2021-birthYear;
+    return age;
+}
+
+const ageCheck = calcAge1(1999); //1999 is the parameter
+console.log(ageCheck);
+
+//Function expression
+const calcAge2 = function(birthYear){
+    return 2021 - birthYear;
+}
+const age2 = calcAge2(1999);
+console.log(ageCheck, age2); */
+
+
+/*Arrow Function
+
+const calcAge4 = birthYear => 2021-birthYear; //Arrow Function
+const calcAge5 = calcAge4(1999);
+console.log(calcAge5);
+
+const yearsUntilRetirement = (birthYear,firstname) => {
+    const age = 2021 - birthYear;
+    const retirementAge = 60 - age;
+    //return retirementAge;
+    return `${firstname} retires in ${retirementAge} years`;
+}
+
+console.log(yearsUntilRetirement(1999, 'naren')); */
